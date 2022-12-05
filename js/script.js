f37ae164b32c92ac956409703c3e6dce9a3387f4
Aleš <mit.mit@seznam.cz>
2022-12-05 11:43:15 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+function imgSlider(tiresClick){
+  document.querySelector(".tire").src = tiresClick;
+}
+function changecircleColor(color){
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
